2cc6408329d72e9823e13d5a6ff6c601ad893749
Tristan Hume <tris.hume@gmail.com>
2013-09-15 13:15:42 -0400

    Empty about box.

---
+++ FinanceLine/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  FinanceLine
+//
+//  Created by Tristan Hume on 2013-09-15.
+//  Copyright (c) 2013 Tristan Hume. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ FinanceLine/AboutViewController.m
@@ -0,0 +1,42 @@
+//
+//  AboutViewController.m
+//  FinanceLine
+//
+//  Created by Tristan Hume on 2013-09-15.
+//  Copyright (c) 2013 Tristan Hume. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (IBAction)closeModal {
+  [self dismissModalViewControllerAnimated:YES];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
