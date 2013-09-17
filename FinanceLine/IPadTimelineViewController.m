dd5e50abad90f78b6b0927fffda833c2a7eadcea
Tristan Hume <tris.hume@gmail.com>
2013-09-17 16:13:44 -0400

    Move iPad stuff to folder.

---
+++ FinanceLine/IPadTimelineViewController.m
@@ -0,0 +1,38 @@
+//
+//  IPadTimelineViewController.m
+//  FinanceLine
+//
+//  Created by Tristan Hume on 2013-09-17.
+//  Copyright (c) 2013 Tristan Hume. All rights reserved.
+//
+
+#import "IPadTimelineViewController.h"
+
+@interface IPadTimelineViewController ()
+
+@end
+
+@implementation IPadTimelineViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
